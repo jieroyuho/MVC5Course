2a3bd8af1e7af053dffeffa1b45eaf89ac0e434d
Tsung-Yu Ho <jieroyuho@gmail.com>
2016-10-29 16:15:02 +0800

    44 練習用 TryUpdateModel 在 Action 裡面執行 Model Binding 並設定 includeProperti

--- MVC5Course/Controllers/ClientsController.cs
+++ MVC5Course/Controllers/ClientsController.cs
@@ -111,11 +111,12 @@ namespace MVC5Course.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ClientId,FirstName,MiddleName,LastName,Gender,DateOfBirth,CreditRating,XCode,OccupationId,TelephoneNumber,Street1,Street2,City,ZipCode,Longitude,Latitude,Notes")] Client client)
+        public ActionResult Edit(int id, FormCollection form)
         {
-            if (ModelState.IsValid)
+            var client = db.Clients.Find(id);
+
+            if (TryUpdateModel(client,null,null, new string[] { "IsAdmin"}))
             {
-                db.Entry(client).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
