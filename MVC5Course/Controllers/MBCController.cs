43b52d1478cd2ec6fdf98202d450f218b4cce707
Tsung-Yu Ho <jieroyuho@gmail.com>
2016-10-29 15:51:12 +0800

    MODEL Binding

--- MVC5Course/Controllers/MBCController.cs
+++ MVC5Course/Controllers/MBCController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 
 namespace MVC5Course.Controllers
 {
-    public class MBCController : Controller
+    public class MBCController : BaseController
     {
         // GET: MBC
         public ActionResult Index()
@@ -33,7 +33,7 @@ namespace MVC5Course.Controllers
         }
 
         [HttpPost]
-        public ActionResult MyForm(ClientLoginViewModel c)
+        public ActionResult MyForm(ClientLoginViewModel c) // MyForm(FormCollection form) 弱型別
         {
             if (ModelState.IsValid)
             {
@@ -47,6 +47,41 @@ namespace MVC5Course.Controllers
         {
             return View();
         }
+
+        public ActionResult ProductList()
+        {
+            var data = db.Products.OrderBy(p => p.ProductId).Take(10);
+            return View(data);
+        }
+
+        public ActionResult BatchUpdate(ProductBatchUpdateViewModel[] items)
+        {
+            /*
+             * 預設輸出的欄位名稱格式：item.ProductId
+             * 要改成以下欄位格式：
+             * items[0].ProductId
+             * items[1].ProductId
+             */
+            if (ModelState.IsValid)
+            {
+                foreach (var item in items)
+                {
+                    var product = db.Products.Find(item.ProductId);
+                    product.ProductName = item.ProductName;
+                    product.Active = item.Active;
+                    product.Stock = item.Stock;
+                    product.Price = item.Price;
+                }
+
+                db.SaveChanges();
+
+                return RedirectToAction("ProductList");
+            }
+
+            return View();
+        }
+
+
 
     }
 }
