d90797f30f88e2eb9b88812f7c4d9cf53a45c52a
Tsung-Yu Ho <jieroyuho@gmail.com>
2016-10-29 14:42:48 +0800

    Action data to View

---
+++ MVC5Course/Controllers/MBCController.cs
@@ -0,0 +1,52 @@
+﻿using MVC5Course.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class MBCController : Controller
+    {
+        // GET: MBC
+        public ActionResult Index()
+        {
+            ViewData["Temp1"] = "暫存資料 Temp1";
+
+            var b = new ClientLoginViewModel()
+            {
+                FirstName = "Will",
+                LastName = "Huang"
+            };
+
+            ViewData["Temp2"] = b;
+
+            ViewBag.Temp3 = b;
+
+            return View();
+        }
+
+        public ActionResult MyForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult MyForm(ClientLoginViewModel c)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["MyFormData"] = c;
+                return RedirectToAction("MyFormResult");
+            }
+            return View();
+        }
+
+        public ActionResult MyFormResult()
+        {
+            return View();
+        }
+
+    }
+}
