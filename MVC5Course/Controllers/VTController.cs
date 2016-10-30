63e9c26db9c9eef89dc466d18fb3c11871d26092
Tsung-Yu Ho <jieroyuho@gmail.com>
2016-10-30 14:21:44 +0800

    Add New Html Layout

--- MVC5Course/Controllers/VTController.cs
+++ MVC5Course/Controllers/VTController.cs
@@ -13,5 +13,9 @@ namespace MVC5Course.Controllers
         {
             return View();
         }
+        public ActionResult NewIndex()
+        {
+            return View();
+        }
     }
 }
